87e296dc71407aee7db82375b03c751520f53476
CodyJReed <cody2j2reed@gmail.com>
2020-10-10 10:14:20 -0700

    Added additional styling and linkage to Header.js

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -31,7 +31,7 @@ const useStyles = makeStyles((theme) => ({
     marginBottom: "3em",
   },
   logo: {
-    height: "7em",
+    height: "8em",
   },
   tabContainer: {
     marginLeft: "auto",
@@ -47,6 +47,12 @@ const useStyles = makeStyles((theme) => ({
     margin: "0 25px 0 50px",
     height: "45px",
   },
+  logoContainer: {
+    padding: 0,
+    "&:hover": {
+      backgroundColor: "transparent"
+    }
+  }
 }));
 
 export default function Header(props) {
@@ -55,12 +61,30 @@ export default function Header(props) {
 
   const handleChange = (e, value) => setValue(value);
 
+  React.useEffect(() => {
+    if (window.location.pathname === "/" && value != 0) {
+      setValue(0)
+    } else if (window.location.pathname === "/services" && value != 1) {
+      setValue(1)
+    }else if (window.location.pathname === "/revolution" && value != 2) {
+      setValue(2)
+    } else if (window.location.pathname === "/about" && value != 3) {
+      setValue(3)
+    } else if (window.location.pathname === "/contact" && value != 4) {
+      setValue(4)
+    } else if (window.location.pathname === "/estimate" && value != 5) {
+      setValue(5)
+    }
+  }, [value])
+
   return (
     <React.Fragment>
       <HideOnScroll>
         <AppBar position="fixed">
           <Toolbar disableGutters>
+            <Button disableRipple component={Link} to="/" className={classes.logoContainer} onClick={() => setValue(0)}>
             <img src={logo} className={classes.logo} />
+            </Button>
             <Tabs
               value={value}
               onChange={handleChange}
@@ -71,7 +95,8 @@ export default function Header(props) {
               <Tab className={classes.tab} component={Link} to="/services" label="Services" />
               <Tab className={classes.tab} component={Link} to="/revolution" label="The Revolution" />
               <Tab className={classes.tab} component={Link} to="/about" label="About Us" />
-              <Tab className={classes.tab} component={Link} to="contact" label="Contact Us" />
+              <Tab className={classes.tab} component={Link} to="/contact" label="Contact Us" />
+              <Tab className={classes.tab} component={Link} to="/estimate" label="Estimate" />
             </Tabs>
             <Button
               variant="contained"
